30d0736206fd62a6ecf5d1ee6717ffc8cd5a5e4f
ShiZhan <g.shizhan.g@gmail.com>
2013-02-09 00:36:59 +0800

    update Windows environment loader script

--- load-dep.cmd
+++ load-dep.cmd
@@ -1,1 +1,1 @@
-@echo Created by Shi.Zhan @ 2013&&@echo setting python libpath for cloned source modules:&&@echo Pyro4&&@set PYTHONPATH=e:\Works\hive\Pyro4\trunk\src;%PYTHONPATH%&&@set PATH=%~dp0;%PATH%
+@echo Created by Shi.Zhan @ 2013&&@echo setting python libpath for cloned source modules:&&@echo Pyro4&&@set PYTHONPATH=e:\Works\hive\Pyro4\trunk\src;e:\Works\hive\isodate\src;e:\Works\hive\rdflib;%PYTHONPATH%&&@set PATH=%~dp0;%PATH%
